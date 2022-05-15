14f4fd3b19a329f076c805fb58dbd3adc61da72c
littlelunar <lunar.inthemoonllight@gmail.com>
2022-05-15 15:43:56 +0700

    fix: PROD

--- frontend/src/env.jsx
+++ frontend/src/env.jsx
@@ -3,8 +3,8 @@ const APP_CONFIG = {
     WEB_API: "http://localhost:8000",
   },
   PROD: {
-    WEB_API: "",
+    WEB_API: "http://api.spa.local",
   },
 };
 
-export const APP_SETTING = APP_CONFIG.DEV;
+export const APP_SETTING = APP_CONFIG.PROD;
