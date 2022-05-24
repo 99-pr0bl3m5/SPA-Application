6916c7377ff88f6ce3d256e37d3a6531ade2984f
littlelunar <lunar.inthemoonllight@gmail.com>
2022-05-24 17:20:52 +0700

    fix: mongo connection

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,17 @@ app.use(bodyPraser.json());
 app.use(cors());
 app.use("/api", endPoint);
 
-mongoose.connect(DB_CONNECTION_STRING, () => {
-  console.log("DB CONNECTED");
-});
+mongoose
+	.connect(DB_CONNECTION_STRING, {
+		useNewUrlParser: true,
+	})
+	.then(() => {
+		console.log('Database Connected successfully');
+	})
+	.catch((err) => {
+		console.log(`Can't Connect To Database ${err}`);
+		process.exit(0);
+	});
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
