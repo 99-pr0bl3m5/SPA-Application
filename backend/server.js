3a5c917b9a54dba373083ded5d8414a603914781
littlelunar <lunar.inthemoonllight@gmail.com>
2022-05-24 10:33:42 +0700

    new: backend allow cors

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const app = express();
 const dotenv = require("dotenv");
 const bodyPraser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 dotenv.config();
 const endPoint = require("./router");
 const PORT = process.env.PORT;
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
+
 app.use(bodyPraser.json());
+app.use(cors());
 app.use("/api", endPoint);
 
 mongoose.connect(DB_CONNECTION_STRING, () => {
