497f9da49bcc9e958603eed1b4c8e4c0c309e471
LittleLunar <lunar.inthemoonllight@gmail.com>
2022-05-25 15:35:34 +0700

    new: whitelist allowed cors

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,6 @@ const endPoint = require("./router");
 const PORT = process.env.PORT;
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
-app.use(bodyPraser.json());
-app.use(cors());
-app.use("/api", endPoint);
-
 mongoose
   .connect(DB_CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -24,6 +20,21 @@ mongoose
     console.log(`Can't Connect To Database ${err}`);
     process.exit(0);
   });
+
+var whitelist = ['http://spa.local']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(bodyPraser.json());
+app.use(cors(corsOptions));
+app.use("/api", endPoint);
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
